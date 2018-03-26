b15d8c7d34acd85b3401ae53ff306cb21242fa0c
home <qu_yh@pku.edu.cn>
2018-03-26 13:36:13 +0800

    init

---
+++ script/baseline.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+test = pd.read_csv('../data/round1_ijcai_18_test_a_20180301.txt',sep=' ',usecols=[0,6])
+train = pd.read_csv('../data/round1_ijcai_18_train_20180301.txt',sep=' ',usecols=[6,26])
+train = train.groupby('item_price_level',as_index=False).mean()
+predict = pd.merge(train, test, on='item_price_level', how='right')
+predict = predict.drop('item_price_level',axis=1)
+predict = predict.reindex(columns=['instance_id', 'is_trade'])
+predict.to_csv('baseline.csv',index=False,sep=' ',header=['instance_id','predicted_score'])
