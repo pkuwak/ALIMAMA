1e7e4e35d1f0d1b7b8a396f828692bcdeb135e81
home <qu_yh@pku.edu.cn>
2018-03-28 15:55:58 +0800

    update
    下载了一个特征工程代码

--- script/baseline.py
+++ script/baseline.py
@@ -5,5 +5,6 @@ train = pd.read_csv('../data/round1_ijcai_18_train_20180301.txt',sep=' ',usecols
 train = train.groupby('item_price_level',as_index=False).mean()
 predict = pd.merge(train, test, on='item_price_level', how='right')
 predict = predict.drop('item_price_level',axis=1)
-predict = predict.reindex(columns=['instance_id', 'is_trade'])
+predict.index = predict['instance_id']
+predict = predict.reindex(index=test['instance_id'], columns=['instance_id', 'is_trade'])
 predict.to_csv('../data/baseline.csv',index=False,sep=' ',header=['instance_id','predicted_score'])
